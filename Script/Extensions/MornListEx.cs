48aa4948477090483d46a2df850f8a9f1d57e3dc
matsufriends <pikapikan0526@gmail.com>
2025-04-21 01:06:35 +0900

    RandomWeightの実装

--- Script/Extensions/MornListEx.cs
+++ Script/Extensions/MornListEx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace MornLib.Extensions
@@ -11,6 +12,40 @@ namespace MornLib.Extensions
             return list[Random.Range(0, list.Count)];
         }
 
+        public static T RandomValue<T>(this IReadOnlyList<T> list, Func<T, float> weightFunc)
+        {
+            var weightSum = 0f;
+            foreach (var t in list)
+            {
+                var weight = weightFunc(t);
+                if (weight <= 0f)
+                {
+                    continue;
+                }
+
+                weightSum += weight;
+            }
+
+            var resultWeight = Random.Range(0f, weightSum);
+            foreach (var t in list)
+            {
+                var weight = weightFunc(t);
+                if (weight <= 0f)
+                {
+                    continue;
+                }
+
+                resultWeight -= weight;
+                if (resultWeight <= 0f)
+                {
+                    return t;
+                }
+            }
+
+            Debug.LogError($"RandomValue failed. weightSum: {weightSum}, resultWeight: {resultWeight}");
+            return list[0];
+        }
+
         public static int MatchCount<T>(this IReadOnlyList<T> list, T correct)
         {
             var count = 0;
@@ -18,7 +53,6 @@ namespace MornLib.Extensions
             for (var i = totalCount - 1; i >= 0; i--)
                 if (EqualityComparer<T>.Default.Equals(list[i], correct))
                     count++;
-
             return count;
         }
 
@@ -29,7 +63,6 @@ namespace MornLib.Extensions
             for (var i = totalCount - 1; i >= 0; i--)
                 if (EqualityComparer<T>.Default.Equals(list[i], correct))
                     count++;
-
             return count;
         }
     }
